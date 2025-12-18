78a8375c39245232e11c7c5508d1bf08702b32a2
Awure Barry <awurebarry@Awures-MacBook-Air.local>
2025-12-18 03:51:12 +0100

    adding env variables

--- web/web/wsgi.py
+++ web/web/wsgi.py
@@ -8,8 +8,17 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import pathlib
+
+import dotenv
 
 from django.core.wsgi import get_wsgi_application
+
+CURRENT_DIR = pathlib.Path(__file__).resolve().parent.parent
+BASE_DIR = CURRENT_DIR.parent
+ENV_FILE_PATH = BASE_DIR / '.env'
+
+dotenv.load_dotenv(dotenv_path=ENV_FILE_PATH)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings')
 
